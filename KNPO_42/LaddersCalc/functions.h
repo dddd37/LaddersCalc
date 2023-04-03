0980a0a1850fe9b09d59ace2c5a4f636d1a39654
dddd37 <dddd45690@gmail.com>
2023-04-03 22:03:29 +0300

    The countOfLadders function is written
    countOfLadders - is a function that solves the main task of counting the number of ladders that can be made up of the number of cubeCount cubes

--- KNPO_42/LaddersCalc/functions.h
+++ KNPO_42/LaddersCalc/functions.h
@@ -1,4 +1,31 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
+#include <iostream>
+
+class Functions
+{
+public:
+    int countOfLadders(int prevStep, int cubeCount);
+};
+
+/*!
+* Cчитает количество лесенок, которые можно сделать из cubeCount.
+*\param[in] prevStep - Количество кубиков в предыдущей ступеньке.
+*\param[in] cubeCount - Общее количество кубиков (1<=cubeCount<=100).
+*\return – Количество лесенок.
+*/
+int Functions::countOfLadders(int prevStep, int cubeCount)
+{
+    if(cubeCount==0) // если количество кубиков равно 0
+    return 1; // лесенку получилось построить
+
+  int count = 0; // обнулить количество лесенок
+  for (int level = 1; level < prevStep; ++level) // для каждой ступеньки лестницы, начиная со второй
+  {
+    count += countOfLadders(level, cubeCount - level); // прибавить к количеству лесенок результат вызова функции подсчета лесенок
+  }
+
+  return count; // вернуть значение количество лесенок
+}
 
 #endif // FUNCTIONS_H
