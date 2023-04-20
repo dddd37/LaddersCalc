6b7f82dc54c0bab42538e7a1a83d11ba1644d13d
dddd37 <dddd45690@gmail.com>
2023-04-20 19:35:59 +0300

    The unpackIntValue function is written
    unpackIntValue - is a function that decompresses an int number if it is at the beginning of the first line without spaces and characters, and also lies in the range from 1 to 100

--- KNPO_42/LaddersCalc/functions.h
+++ KNPO_42/LaddersCalc/functions.h
@@ -1,12 +1,84 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 #include <iostream>
+#include <QFile>
+#include <QString>
+#include <QTextStream>
 
 class Functions
 {
 public:
+    int unpackIntValue(QFile& input, QString outputFileName);
     int countOfLadders(int prevStep, int cubeCount);
 };
+
+/*!
+* Распаковывает число N из текстового файла в диапазоне(1<=N<=100).
+*\param[in] input – Входной файл.
+*\param[in] outputFileName – Путь или имя выходного файла.
+*\return – Число от 1 до 100, если оно соответствует условиям, иначе NULL.
+*/
+int Functions::unpackIntValue(QFile& input, QString outputFileName)
+{
+    QFile output(outputFileName); // создание или чтение существующего файла
+    output.open(QIODevice::WriteOnly | QIODevice::Text);
+
+    QTextStream in(&input); // создание объекта QTextStream для чтения данных из входного файла
+
+//----------настройка вывода русского текста в формате UTF-8 в файл-----------
+    QTextStream out(&output);
+    out.setCodec("UTF-8");
+    QString inputLine = in.readLine();
+    QString russianText = NULL;
+//-------------------------проверка содержания входного файла----------------------------
+    if (inputLine.isEmpty()) // если входной файл пустой
+    {
+        // выдать ошибку в выходной файл
+        russianText = "Файл с входными данными является пустым!";
+        out << russianText;
+        output.close();
+        return NULL;
+    }
+    if(inputLine.size() > 3) // если строка длиннее 3ёх символов
+    {
+        // выдать ошибку в выходной файл
+        russianText = "Неверные входные данные! В строке не может быть больше трёх символов!";
+        out << russianText;
+        output.close();
+        return NULL;
+    }
+    bool areDigits = true; // посимвольная проверка строки на наличие недопустимых символов
+    for (const QChar& ch : inputLine) {
+            if (!ch.isDigit()) {
+                areDigits = false;
+            }
+        }
+    if(areDigits == false) // если строка содержит не только цифры
+    {
+        // выдать ошибку в выходной файл
+        russianText = "Неверные входные данные! В строке не должно быть символов, не являющихся цифрами!";
+        out << russianText;
+        output.close();
+        return NULL;
+    }
+//-----------------преобразование строки в значение int----------------
+    int value = inputLine.toInt();
+    if(value < 1 || value > 100) // если значение int не лежит в заданном диапазоне
+    {
+        // выдать ошибку в выходной файл
+        russianText = "Неверные входные данные! Число N не лежит в заданном диапазоне (1<=N<=100)";
+        out << russianText;
+        output.close();
+        input.close();
+        return NULL;
+    }
+    else
+    {
+        // вернуть значение из файла
+        input.close();
+        return value;
+    }
+}
 
 /*!
 * Cчитает количество лесенок, которые можно сделать из cubeCount.
