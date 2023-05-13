60ab4c1ecefb7c52dd74d61b88a0d92e43a65aa3
dddd37 <dddd45690@gmail.com>
2023-05-13 19:03:21 +0300

    A subproject of autotesting has been created

---
+++ KNPO_42/kinpoTests/tst_tests.cpp
@@ -0,0 +1,35 @@
+#include <QtTest>
+
+// add necessary includes here
+
+class Tests : public QObject
+{
+    Q_OBJECT
+
+public:
+    Tests();
+    ~Tests();
+
+private slots:
+    void test_case1();
+
+};
+
+Tests::Tests()
+{
+
+}
+
+Tests::~Tests()
+{
+
+}
+
+void Tests::test_case1()
+{
+
+}
+
+QTEST_APPLESS_MAIN(Tests)
+
+#include "tst_tests.moc"
