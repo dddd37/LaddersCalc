9c81fadf060b9ce753fd73b64640f82f22afcd03
dddd37 <dddd45690@gmail.com>
2023-06-10 00:04:36 +0300

    Changed the main function
    Instead of a set of conditions with return 0, the try-catch construction is implemented

--- KNPO_42/LaddersCalc/main.cpp
+++ KNPO_42/LaddersCalc/main.cpp
@@ -9,71 +9,76 @@ Functions func;
  */
 int main(const int argc, char **argv)
 {
-    QTextStream outStream(stdout);
-    outStream.setCodec(QTextCodec::codecForName("cp866"));
+    setlocale(LC_ALL, "Russian");
+    QTextStream outStream(stdout); // переменная для вывода текста в консоль
 
-//-------------проверка правильности запуска программы-----------------
-   if (argc != 3) // если количество аргументов в командной строке не 3
-   {
-       // выдать ошибку в консоль
-       outStream << QString("Попробуйте ещё раз! Правильный шаблон запуска: LaddersCalc.exe ./input.txt ./output.txt\nТакже можно указать полный путь к файлам.") << flush;
-       return 0;
-   }
-   if (argv[1] == NULL || argv[2] == NULL)
-   {
-       // выдать ошибку в консоль
-       outStream << QString("Попробуйте ещё раз! Правильный шаблон запуска: LaddersCalc.exe ./input.txt ./output.txt\nТакже можно указать полный путь к файлам.") << flush;
-       return 0;
-   }
-//-------------путь к входному файлу--------------
-    QString inputFileName = argv[1];
-    QFile inputFile(inputFileName);
-//-------------путь к выходному файлу-------------
-    QString outputFileName = argv[2];
-    QFile outputFile(outputFileName);
-//-----------------проверка входного файла-------------------------
-   if(!inputFileName.endsWith(".txt")) // если расширение входного файла не .txt
-   {
-       // выдать ошибку в консоль
-       outStream << QString("Формат входного файла не поддерживается! Правильный формат: .txt") << flush;
-       return 0;
-   }
-   if(!inputFile.exists() || !inputFile.open(QIODevice::ReadOnly)) // если входной файл не существует или не открыт для чтения
-   {
-       // выдать ошибку в консоль
-       outStream << QString("Файл с входными данными не существует или нет доступа для чтения этого файла!") << flush;
-       return 0;
-   }
-//-----------------проверка выходного файла------------------------
-   if(!outputFile.exists()) // если выходного файла не существует
-   {
-       // задать имя для создания нового выходного файла
-       outputFileName = "new_output.txt";
-   }
-   else
-   {
-       if(!outputFileName.endsWith(".txt")) // если расширение выходного файла не .txt
-       {
-           // выдать ошибку в консоль
-           outStream << QString("Формат выходного файла не поддерживается! Правильный формат: .txt") << flush;
-           return 0;
-       }
-       if(outputFile.exists() && !outputFile.open(QIODevice::WriteOnly)) // если выходной файл существует, но не открыт для записи
-       {
-           // выдать ошибку в консоль
-           outStream << QString("Файл для выходных данных существует, но нет доступа для записи в этот файл!") << flush;
-           return 0;
-       }
-   }
-    int cubeCount =  func.unpackIntValue(inputFile, outputFileName); // распаковка количества кубиков из входного файла
-    if(cubeCount == NULL)
+    try
     {
-        return 0;
+    //-------------проверка правильности запуска программы-----------------
+       if (argc != 3) // если количество аргументов в командной строке не 3
+       {
+           // выдать ошибку в консоль
+           throw QString("Попробуйте ещё раз! Правильный шаблон запуска: LaddersCalc.exe ./input.txt ./output.txt\nТакже можно указать полный путь к файлам.");
+           return 0;
+       }
+       if (argv[1] == NULL || argv[2] == NULL)
+       {
+           // выдать ошибку в консоль
+           throw QString("Попробуйте ещё раз! Правильный шаблон запуска: LaddersCalc.exe ./input.txt ./output.txt\nТакже можно указать полный путь к файлам.");
+           return 0;
+       }
+    //---------------------путь к входному файлу-----------------------
+        QString inputFileName = argv[1];
+        QFile inputFile(inputFileName);
+    //---------------------путь к выходному файлу----------------------
+        QString outputFileName = argv[2];
+        QFile outputFile(outputFileName);
+    //-----------------проверка входного файла-------------------------
+       if(!inputFileName.endsWith(".txt")) // если расширение входного файла не .txt
+       {
+           // выдать ошибку в консоль
+           throw QString("Формат входного файла не поддерживается! Правильный формат: .txt");
+       }
+       if(!inputFile.exists() || !inputFile.open(QIODevice::ReadOnly)) // если входной файл не существует или не открыт для чтения
+       {
+           // выдать ошибку в консоль
+           throw QString("Файл с входными данными не существует или нет доступа для чтения этого файла!");
+       }
+    //-----------------проверка выходного файла------------------------
+       if(!outputFile.exists()) // если выходного файла не существует
+       {
+           // задать имя для создания нового выходного файла
+           outputFileName = "new_output.txt";
+       }
+       else
+       {
+           if(!outputFileName.endsWith(".txt")) // если расширение выходного файла не .txt
+           {
+               // выдать ошибку в консоль
+               throw QString("Формат выходного файла не поддерживается! Правильный формат: .txt");
+           }
+           if(outputFile.exists() && !outputFile.open(QIODevice::WriteOnly)) // если выходной файл существует, но не открыт для записи
+           {
+               // выдать ошибку в консоль
+               throw QString("Файл для выходных данных существует, но нет доступа для записи в этот файл!");
+           }
+       }
+        int cubeCount =  func.unpackIntValue(inputFile, outputFileName); // распаковка количества кубиков из входного файла
+        if(cubeCount == NULL)
+        {
+            throw QString("");
+        }
+        else
+        {
+            int laddersCount = func.countOfLadders(cubeCount+1, cubeCount); // подсчёт количества лесенок
+            func.outputIntValue(outputFileName,laddersCount); // вывод количества лесенок
+        }
     }
-    else
+    // обработка исключения
+    catch (const QString& errorText)
     {
-        int laddersCount = func.countOfLadders(cubeCount+1, cubeCount); // подсчёт количества лесенок
-        func.outputIntValue(outputFileName,laddersCount); // вывод количества лесенок
+        // вывести исключение в консоль
+        outStream << errorText << flush;
     }
     return 0;
 }
